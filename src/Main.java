77c140edcb269b1e7736b2cdf5a0c496e6293977
Nicolly Candida <nicolly.candida2005@gmail.com>
2025-11-13 14:40:47 -0300

    reparaçoesFINAISS

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
                                             // Lançada se a Thread for interrompida enquanto o Main espera com join()
     public static void main(String[] args) throws InterruptedException {
